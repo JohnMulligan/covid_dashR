4672496cdf2a24ef94a2050d0017d1168239f7f0
jcm10+1-at-391170303014 <john.connor.mulligan@gmail.com>
2022-02-23 17:46:18 -0600

    ga

---
+++ assets/custom-script.js
@@ -0,0 +1,7 @@
+app.scripts.config.serve_locally = False
+app.scripts.append_script({
+‘external_url’: ‘https://www.googletagmanager.com/gtag/js?id=G-0QM0HEDDPT’
+})
+app.scripts.append_script({
+‘external_url’: ‘https://cdn.jsdelivr.net/gh/lppier/lppier.github.io/gtag.js’
+})
